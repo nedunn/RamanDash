14d39e734e7c60c20b500e64861944816d212449
dunn <ndunn6@vols.utk.edu>
2023-02-07 18:54:46 -0500

    Added submit button, in progress adding clear button

--- app.py
+++ app.py
@@ -53,7 +53,10 @@ app.layout = html.Div([
               dcc.Input(id='peaks', placeholder='comma separated values')
             #html.Button('Update Plot', id='submit'),
             ],
-            style={'margin-top':'10px'})
+            style={'margin-top':'10px'}),
+    
+    html.Div([html.Button('Update Plot',id='submit')])
+    #html.Div([html.Button('Clear',id='clear')])
 ])
 
 #__________
@@ -84,18 +87,27 @@ def parse_contents(content_string):
     decoded = pd.read_csv(io.StringIO(base64.b64decode(content_string).decode('utf-8')))
     return decoded
 
+# @app.callback(Output('output-data-upload', 'children'),
+#               [Input('upload-data', 'contents'),
+#                Input('upload-data','filename'),
+#                Input('title-input','value'),
+#                Input('details','value'),
+#                Input('title-size','value'),
+#                Input('peaks','value')])
+
 @app.callback(Output('output-data-upload', 'children'),
               [Input('upload-data', 'contents'),
                Input('upload-data','filename'),
-               Input('title-input','value'),
-               Input('details','value'),
-               Input('title-size','value'),
-               Input('peaks','value')])
+               Input('submit','n_clicks')],
+               [State('title-input','value'),
+               State('details','value'),
+               State('title-size','value'),
+               State('peaks','value')])
 
 #__________
 #Apply Update
 #__________
-def update_output(list_of_contents, list_of_names,title,details,tsize,peaks):
+def update_output(list_of_contents, list_of_names,n_clicks,title,details,tsize,peaks):
     if list_of_contents is not None:
         num=len(list_of_contents) #Number of files uploaded
         fig=sp.make_subplots(rows=num,cols=1,vertical_spacing=0, #initialize figure
@@ -125,12 +137,50 @@ def update_output(list_of_contents, list_of_names,title,details,tsize,peaks):
         
         children.append(dcc.Graph(id='graph',figure=fig))
         return children
+
+
     
 #__________
 #Run it!
 #__________
 if __name__ == '__main__':
     app.run_server(debug=True)
+
+
+
+#__________
+#multip call backs
+#__________
+# app = dash.Dash()
+
+# app.layout = html.Div([
+#     html.Div([
+#         dcc.Input(id='title-input', type='text', value='Default Title'),
+#     ]),
+#     html.Button('Submit', id='submit'),
+#     html.Button('Clear', id='clear'),
+#     html.Div(id='output-data-upload'),
+# ])
+
+# @app.callback(
+#     Output(component_id='output-data-upload', component_property='children'),
+#     [Input(component_id='submit', component_property='n_clicks')],
+#     [State(component_id='title-input', component_property='value')]
+# )
+# def update_output(n_clicks, value):
+#     return html.Div([
+#         html.H1(value)
+#     ])
+
+# @app.callback(
+#     Output(component_id='title-input', component_property='value'),
+#     [Input(component_id='clear', component_property='n_clicks')]
+# )
+# def clear_title(n_clicks):
+#     return ''
+
+# if __name__ == '__main__':
+#     app.run_server()
 
 
 #___Attempt at adding traces___
