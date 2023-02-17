161ec19fecd46c11527796b36400e3b00dac8b6f
dunn <ndunn6@vols.utk.edu>
2023-02-17 11:32:05 -0500

    New layout + subplot height option

--- app.py
+++ app.py
@@ -20,77 +20,82 @@ set y axis range'''
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 ######################################################
-#Layout Functions
+#Components
 ######################################################
 
-def slider_layout():
-    return {'placement':'bottom','always_visible':True}#,{'padding':20,'flex':1}#{'width':'50%','display':'flex',}
+slider_layout = {'placement':'bottom','always_visible':True}#,{'padding':20,'flex':1}#{'width':'50%','display':'flex',}
+indiv_style={'padding':20,'flex':1} #input_style
+row_style={'display':'flex', 'flex-direction':'row'}
 
-def upload():
-    return html.Div(children=[dcc.Upload(id='upload-data',
+upload=html.Div(children=[dcc.Upload(id='upload-data',
                       children=html.Div(['Drag and Drop or ', html.A('Select Files')]),
                       multiple=True,style={'width': '100%','height': '30px',
                                            'lineHeight': '30px','borderWidth': '1px',
                                            'borderStyle': 'dashed','borderRadius': '5px',
-                                           'textAlign': 'center','margin': '1px'}),
-            html.Div(id='output-data-upload')])
+                                           'textAlign': 'center','margin': '1px'})
+            ])
             #dcc.Graph(id='graph',figure=go.Figure())])
 
-def input_style():
-    return{'padding':20,'flex':1}
-
-def input1():
-    return html.Div(children=[
+comp_title = html.Div([
         html.Label('Set Title: '),
-        dcc.Input(id='title-input',placeholder=''),
+        dcc.Input(id='title-input', size='30',placeholder='')],style={'padding':20,'flex':1})
+comp_details=html.Div([
+    html.Label('Enter Raman collection details: '),
+    dcc.Input(id='details', size='30',placeholder='532nm, 10mW, 40X, 60s')],style={'padding':20,'flex':1})
+comp_peak_list=html.Div([
+    html.Label('Enter Raman peaks: '),
+    dcc.Input(id='peaks',size='60',placeholder='comma separated values')],style={'padding':20,'flex':1})
 
-        html.Br(),
+
+
+sec1=html.Div([comp_title,comp_details,comp_peak_list], style={'display':'flex', 'flex-direction':'row'})
+
+display_graph=html.Div([html.Div(id='output-data-upload')])
+
+def sec2A():
+    return html.Div(children=[
 
         html.Label('Set Title Size: '),
         dcc.Slider(10,70,value=40, id='title-size',
-                   tooltip=slider_layout()),
-        html.Div(id='container'),
-
-        html.Br(),html.Br(),
-
-        html.Label('Enter Raman Spectra details: '),
-        dcc.Input(id='details',value=''),
+                   tooltip=slider_layout),
         
         html.Br(),
 
-        html.Label('Set detail size: '),
-        dcc.Slider(10,30,value=20,id='det-size',tooltip=slider_layout()),
+        html.Label('Set Detail Text size: '),
+        dcc.Slider(10,30,value=20,id='det-size',tooltip=slider_layout),
 
         html.Br(),
 
         html.Label('Set detail x,y location: '),
-        dcc.Slider(-1,1.5,value=0.95,id='det-ax',tooltip=slider_layout()),
-        dcc.Slider(-1,2,value=1.1,id='det-ay',tooltip=slider_layout())
+        dcc.Slider(-1,1.5,value=0.95,id='det-ax',tooltip=slider_layout),
+        dcc.Slider(-1,2,value=1.1,id='det-ay',tooltip=slider_layout)
 
-    ],style=input_style())
+    ],style=indiv_style)
 
-def input2():
+def sec2B():
     return html.Div(children=[
-        html.Label('Enter Raman peaks: '),
-        dcc.Input(id='peaks',placeholder='comma separated values'),
+
         html.Label('Set peak label size: '),
-        dcc.Slider(10,30,value=20,id='peak-size',tooltip=slider_layout()),
+        dcc.Slider(10,30,value=20,id='peak-size',tooltip=slider_layout),
         html.Label('Set peak label height: '),
-        dcc.Slider(-1,2,value=1.2,id='peak-height',tooltip=slider_layout()),
+        dcc.Slider(-1,2,value=1.2,id='peak-height',tooltip=slider_layout),
 
-        html.Br(),html.Br(),
-
+        html.Br(),
+        html.Label('Set subplot height: '),
+        dcc.Slider(100,2000,value=500,id='subplot-height',tooltip=slider_layout),
+        html.Br(),
+        
         html.Label('Set legend text size: '),
-        dcc.Slider(10,30,value=20,id='legend',tooltip=slider_layout())
+        dcc.Slider(10,30,value=20,id='legend',tooltip=slider_layout)
         ],
-        style=input_style())
+        style=indiv_style)
 
 def build_inputs():
-    return html.Div(html.Div([input1(),input2()],
+    return html.Div(html.Div([sec2A(),sec2B()],
                     style={'display':'flex','flex-direction':'row'}))
 
 def build_layout():
-    return html.Div([upload(),build_inputs()])
+    return html.Div([upload,sec1,display_graph,build_inputs()])
 
 ######################################################
 #Handle input
@@ -131,6 +136,7 @@ app.layout = build_layout()
     Input('peak-size','value'),
     Input('peak-height','value'),
     Input('legend','value'),
+    Input('subplot-height','value'),
     Input('title-input','value'),
     Input('details','value'),
     Input('peaks','value')]
@@ -139,7 +145,7 @@ app.layout = build_layout()
 
 
 def update_output(list_of_contents,list_of_filenames, #Input from Files
-                  tsize,dsize,dx,dy,psize,ph,lsize, #Input from user variables
+                  tsize,dsize,dx,dy,psize,ph,lsize,subh, #Input from user variables
                   title,details,peaks #State inputs
                   ):
     
@@ -170,6 +176,7 @@ def update_output(list_of_contents,list_of_filenames, #Input from Files
                           legend=dict(yanchor='bottom',y=0),
                           legend_font_size=lsize)
                           #height=fig_h, width=fig_w)
+        fig['layout'].update(height=subh)
 
         if peaks is not None:
             numbers=[int(num) for num in peaks.split(',')]
